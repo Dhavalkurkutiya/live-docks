2b1ba48dcaf08c3342f56c91099c2c0174a0f515
Dhaval Kurkutuiya <Kurkutiyadhaval30@gmail.com>
2024-07-20 14:05:30 +0530

    04:Authenticate User with Real-time Features

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -6,20 +6,7 @@ import React from "react";
 const Document = () => {
   return (
     <div>
-      <Header>
-        <div className="flex w-fit items-center justify-center gap-2">
-          <p className="document-title">This fake document title</p>
-        </div>
-        <SignedOut>
-          {" "}
-          <SignInButton />{" "}
-        </SignedOut>{" "}
-        <SignedIn>
-          {" "}
-          <UserButton />{" "}
-        </SignedIn>
-      </Header>
-      <Editor />
+    
     </div>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import Provider from "./Provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -40,7 +41,9 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
+          <Provider>
           {children}
+          </Provider>
         </body>
       </html>
     </ClerkProvider>
